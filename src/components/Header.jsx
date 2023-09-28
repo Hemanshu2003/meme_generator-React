3da79d5a998de65191282cc618847028e9fc6b73
Hemanshu2003 <113788603+Hemanshu2003@users.noreply.github.com>
2023-09-28 17:44:58 +0530

    xc

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ export default function Header() {
     <header className="header">
       <img src={headerImg} className="header--image" />
       <h2 className="header--title">Meme Generator</h2>
-      <h4 className="header--project">React Course - Project 3</h4>
+      <h4 className="header--project">by Hemanshu Waghmare</h4>
     </header>
   );
 }
